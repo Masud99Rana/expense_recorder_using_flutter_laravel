00b1517959915d38b4bd2b1e213df8eb29b7006a
Masud99Rana <MasudRana917775@gmail.com>
2022-08-10 09:26:03 +0600

    restucture folder

--- my_app/lib/screens/categories.dart
+++ my_app/lib/screens/categories.dart
@@ -1,19 +1,7 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
-class Category {
-  int id;
-  String name;
-
-  Category({required this.id, required this.name});
-
-  factory Category.fromJson(Map<String, dynamic> json) {
-    return Category(id: json['id'], name: json['name']);
-  }
-}
+import 'package:my_app/models/category.dart';
+import 'package:my_app/services/api.dart';
+import 'package:my_app/widgets/CategoryEdit.dart';
 
 class Categories extends StatefulWidget {
   @override
@@ -22,119 +10,49 @@ class Categories extends StatefulWidget {
 
 class _CategoriesState extends State<Categories> {
   late Future<List<Category>> futureCategories;
-  final _formKey = GlobalKey<FormState>();
-  late Category selectedCategory;
-  final categoryNameController = TextEditingController();
-
-  Future<List<Category>> fetchCategories() async {
-    http.Response response = await http
-        .get(Uri.parse('http://flutter-api.laraveldaily.com/api/categories'));
-
-    List categories = jsonDecode(response.body);
-
-    return categories.map((category) => Category.fromJson(category)).toList();
-  }
-
-  Future saveCategory() async {
-    final form = _formKey.currentState;
-
-    if (!form!.validate()) {
-      return;
-    }
-
-    String uri = 'http://flutter-api.laraveldaily.com/api/categories/' +
-        selectedCategory.id.toString();
-
-    await http.put(Uri.parse(uri),
-        headers: {
-          HttpHeaders.contentTypeHeader: 'application/json',
-          HttpHeaders.acceptHeader: 'application/json',
-        },
-        body: jsonEncode({'name': categoryNameController.text}));
-
-    Navigator.pop(context);
-  }
+  ApiService apiService = ApiService();
 
   @override
   void initState() {
     super.initState();
-    futureCategories = fetchCategories();
+    futureCategories = apiService.fetchCategories();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Categories'),
-      ),
-      body: FutureBuilder<List<Category>>(
-        future: futureCategories,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                Category category = snapshot.data![index];
-                return ListTile(
-                  title: Text(category.name),
-                  trailing: IconButton(
-                    icon: Icon(Icons.edit),
-                    onPressed: () {
-                      selectedCategory = category;
-                      categoryNameController.text = category.name;
-                      showModalBottomSheet(
-                          context: context,
-                          isScrollControlled: true,
-                          builder: (context) {
-                            return Padding(
-                                padding: EdgeInsets.all(10),
-                                child: Form(
-                                    key: _formKey,
-                                    child: Column(children: <Widget>[
-                                      TextFormField(
-                                        controller: categoryNameController,
-                                        validator: (String? value) {
-                                          if (value!.isEmpty) {
-                                            return 'Enter category name';
-                                          }
-
-                                          return null;
-                                        },
-                                        decoration: InputDecoration(
-                                          border: OutlineInputBorder(),
-                                          labelText: 'Category name',
-                                        ),
-                                      ),
-                                      Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: <Widget>[
-                                            ElevatedButton(
-                                              child: Text('Save'),
-                                              onPressed: () => saveCategory(),
-                                            ),
-                                            ElevatedButton(
-                                              style: ElevatedButton.styleFrom(
-                                                  primary: Colors.red),
-                                              child: Text('Cancel'),
-                                              onPressed: () =>
-                                                  Navigator.pop(context),
-                                            ),
-                                          ])
-                                    ])));
-                          });
-                    },
-                  ),
+        appBar: AppBar(
+          title: Text('Categories'),
+        ),
+        body: FutureBuilder<List<Category>>(
+            future: futureCategories,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    Category category = snapshot.data![index];
+                    return ListTile(
+                      title: Text(category.name),
+                      trailing: IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                          showModalBottomSheet(
+                              context: context,
+                              isScrollControlled: true,
+                              builder: (context) {
+                                return CategoryEdit(category);
+                              });
+                        },
+                      ),
+                    );
+                  },
                 );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Text('Something went wrong');
-          }
+              } else if (snapshot.hasError) {
+                return Text('Something went wrong');
+              }
 
-          return CircularProgressIndicator();
-        },
-      ),
-    );
+              return CircularProgressIndicator();
+            }));
   }
 }
---
+++ my_app/lib/widgets/CategoryEdit.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/models/category.dart';
+import 'package:my_app/services/api.dart';
+
+class CategoryEdit extends StatefulWidget {
+  final Category category;
+
+  CategoryEdit(this.category, {Key? key}) : super(key: key);
+
+  @override
+  _CategoryEditState createState() => _CategoryEditState();
+}
+
+class _CategoryEditState extends State<CategoryEdit> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final categoryNameController = TextEditingController();
+  ApiService apiService = ApiService();
+  String errorMessage = '';
+
+  @override
+  void initState() {
+    categoryNameController.text = widget.category.name;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(10),
+        child: Form(
+            key: _formKey,
+            child: Column(children: <Widget>[
+              TextFormField(
+                controller: categoryNameController,
+                validator: (String? value) {
+                  if (value!.isEmpty) {
+                    return 'Enter category name';
+                  }
+
+                  return null;
+                },
+                onChanged: (text) => setState(() => errorMessage = ''),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Category name',
+                ),
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    ElevatedButton(
+                      child: Text('Save'),
+                      onPressed: () => saveCategory(),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(primary: Colors.red),
+                      child: Text('Cancel'),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                  ]),
+              Text(errorMessage, style: TextStyle(color: Colors.red))
+            ])));
+  }
+
+  Future saveCategory() async {
+    final form = _formKey.currentState;
+
+    if (!form!.validate()) {
+      return;
+    }
+
+    apiService
+        .updateCategory(widget.category.id, categoryNameController.text)
+        .then((Category category) => Navigator.pop(context))
+        .catchError((exception) {
+      setState(() {
+        errorMessage = exception.toString();
+      });
+    });
+  }
+}
